64ced300d6660f2caf55992f6265c93aca7a18e3
gamecoder-nz <42922238+gamecoder-nz@users.noreply.github.com>
2024-11-15 10:05:19 +1300

    Added game variables and constants

--- Adventure/Adventure.py
+++ Adventure/Adventure.py
@@ -1,1 +1,25 @@
+# imports
+import random
+# game variables and constants
+game_over = False
+MAX_PLAYER_HITPOINTS = 100
+player_location = ""
+player_hitpoints = MAX_PLAYER_HITPOINTS
+player_attack_rating = 1
+player_defence_rating = 1
 
+key_location = "rocks"
+shield_location = "skeleton"
+potion_location = "wizard"
+sword_location = "house"
+
+ogre_hitpoints = 20
+ogre_attack_rating = 10
+ogre_defence_rating = 20
+
+house_door_unlocked = False
+# command handlers
+
+# location functions
+
+# main game loop
