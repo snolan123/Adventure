eb77a85254fc494418908a56e64e643a51cc6651
snolan123 <snolan@horowhenua.school.nz>
2025-03-25 15:14:32 +1300

    Nearly finished

--- Adventure/v2.py
+++ Adventure/v2.py
@@ -1,20 +1,24 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, attack_rating, defence_rating):
         self.name = name
         self.items = []
         self.location = 0
+        self.attack_rating = attack_rating
+        self.defence_rating = defence_rating
 
     def move(self, direction):
         if direction in self.location.exits and self.location.exits[direction].canMove() == True:
             self.location = self.location.exits[direction]
             print(f"You move {direction} to a {self.location.name}.")
             self.location.describe()
+            self.location.moveTo(self)
         else:
             print("You can't go that way.")
 
     def pickup(self, item_name):
         for item in self.location.items:
             if item.name == item_name:
+                item.pickup(player)
                 self.items.append(item)
                 self.location.items.remove(item)
                 print(f"You picked up a {item.name}.")
@@ -25,6 +29,7 @@ class Player:
     def drop(self, item_name):
         for item in self.items:
             if item.name == item_name:
+                item.drop(player)
                 self.location.items.append(item)
                 self.items.remove(item)
                 print(f"You dropped a {item.name}.")
@@ -54,6 +59,32 @@ class Item:
     def use(self, player):
         pass
 
+class Sword(Item):
+    def __init__(self):
+        Item.__init__(self, "sword")
+
+    def pickup(self, player):
+        player.attack_rating += 30
+
+    def drop(self, player):
+        player.attack_rating -= 30
+
+class Potion(Item):
+    def __init__(self):
+        Item.__init__(self, "potion")
+
+    def use(self, player):
+        player.hitpoints += 30
+
+class Shield(Item):
+    def __init__(self):
+        Item.__init__(self, "shield")
+
+    def pickup(self, player):
+        player.defence_rating += 30
+
+    def drop(self, player):
+        player.defence_rating -= 30
 
 class Monster:
     def __init__(self, name, description, hitpoints, attack_rating, defence_rating):
@@ -85,6 +116,9 @@ class Location:
 
     def canMove(self):
         return True
+
+    def moveTo(self, player):
+        pass
 
     def examine(self, item):
         pass
@@ -129,17 +163,65 @@ class HouseEntrance(Location):
         else:
             print("Nothing happens.")
 
+class Forest(Location):
+    def __init__(self, name, description):
+        Location.__init__(self, name, description)
+        self.examined_skeleton = False
+
+    def describe(self):
+        Location.describe(self)
+        print("There is a skeleton of an old soldier here")
+
+    def examine(self, item):
+        if item == "skeleton":
+            if self.examined_skeleton == False:
+                print("There is a shield here")
+                self.items.append(Shield())
+                self.examined_skeleton = True
+            else:
+                print("There is nothing here")
+        else:
+            print(f"There is no {item} here")
+
+class Tower(Location):
+    def __init__(self, name, description):
+        Location.__init__(self, name, description)
+        self.has_wizard = True
+
+    def describe(self):
+        Location.describe(self)
+
+        if self.has_wizard == True:
+            print("Before you stands a wise wizard in deep purple robes, their staff crackling with mystical energy as they gaze at you with an air of both mystery and power.")
+
+    def moveTo(self, player):
+        if self.has_wizard == True:
+            print("I have a riddle for you bold adventurer.")
+            answer = input("I am tall when I am young and I am short when I am old. What am I? ").lower().strip()
+            if answer == "candle":
+                print("Curses!")
+                print("Here is a health potion to help with your journey.")
+                print("With a puff of smoke the wizard disappears.")
+                player.items.append(Potion())
+                self.has_wizard = False
+            else:
+                print("Wrong!")
+                print("With a wave of his hand he blasts you out the door.")
+                player.location = self.location.exits["outside"]
+                player.location.describe()
+                player.location.moveTo()
+
 river_1 = Location("river", "The crystal-clear river flows gently, its waters sparkling in the sunlight, with tall grasses swaying along the banks.")
 cave = Location("cave", "Inside the cave, the air is cool and damp, with jagged rocks lining the walls and a faint, eerie glow emanating from deep within the shadows.")
 cave_entrance = Location("cave entrance", "The dark cave entrance looms ominously, with a cool, damp air and faint echoes beckoning you inside.")
 field = Location("field", "A vast, open field stretches before you, dotted with colorful wildflowers and swaying grasses beneath a bright sky.")
 river_with_rocks = RiverWithRocks("river", "The crystal-clear river flows gently, its waters sparkling in the sunlight, with tall grasses swaying along the banks.")
 meadow = Location("meadow", "The meadow is a peaceful haven, with soft grass underfoot, vibrant wildflowers blooming in every color, and the air filled with the sweet scent of nature and the hum of bees.")
-house = Location("house", "The inside of the house is cozy, with a warm fire crackling in the hearth, wooden furniture scattered around, and the smell of fresh-baked bread lingering in the air.")
-house_entrance = Location("house entrance", "A quaint cottage with weathered stone walls and a wooden door slightly ajar, surrounded by an overgrown garden and a stone path.")
+house = House("house", "The inside of the house is cozy, with a warm fire crackling in the hearth, wooden furniture scattered around, and the smell of fresh-baked bread lingering in the air.")
+house_entrance = HouseEntrance("house entrance", "A quaint cottage with weathered stone walls and a wooden door slightly ajar, surrounded by an overgrown garden and a stone path.")
 river_3 = Location("river", "The crystal-clear river flows gently, its waters sparkling in the sunlight, with tall grasses swaying along the banks.")
-forest = Location("forest", "The dense forest is filled with towering trees, soft moss underfoot, and the rich scent of pine, with only the rustling of leaves breaking the silence.")
-tower = Location("tower", "The wizard's tower is filled with ancient books, strange glowing potions on shelves, and a mystical aura that seems to hum with arcane energy.")
+forest = Forest("forest", "The dense forest is filled with towering trees, soft moss underfoot, and the rich scent of pine, with only the rustling of leaves breaking the silence.")
+tower = Tower("tower", "The wizard's tower is an ancient, towering structure filled with shelves of arcane tomes, glowing potions, and strange artifacts.")
 tower_entrance = Location("tower entrance", "The towering stone archway of the ancient tower stands before you, its oak door reinforced with iron and a sense of mystery in the air.")
 tower_entrance.building = tower
 
@@ -160,6 +242,8 @@ tower.exits = {"outside": tower_entrance}
 ogre = Monster("ogre", "The ogre stands towering before you, its hulking figure covered in rough, mossy skin, with wild, unkempt hair and glowing eyes that burn with a fierce, menacing glare.", 100, 10, 20)
 cave.monster = ogre
 
+house.items.append(Sword())
+
 name = ""
 
 while name == "":
@@ -167,7 +251,7 @@ while name == "":
 
 print(f"Welcome brave {name}")
 
-player = Player(name)
+player = Player(name, 1, 1)
 player.location = meadow
 player.location.describe()
 
@@ -182,3 +266,7 @@ while True:
         player.location.examine(commands[1])
     elif commands[0] == "pickup":
         player.pickup(commands[1])
+    elif commands[0] == "drop":
+        player.drop(commands[1])
+    elif commands[0] == "use":
+        player.use(commands[1])
